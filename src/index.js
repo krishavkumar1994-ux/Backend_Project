4e9464860e28202347fc735b12097a374d877495
krishavkumar1994-ux <krishavkumar1994@gmail.com>
2026-09-06 22:58:15 +0530

    Api Response and Error Handling

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+await connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("MONGO db connection failed !!! ", error)
+})
+
+
+import express from "express"
+const app = express()
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.
+            MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`APP IS LISTENING ON PORT ${process.env.PORT}`);
+        })
+    } catch(error){
+        console.error("ERROR: ", error)
+        throw error
+    }
+})()
+
+
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+//Recommended method using Promise
+const asyncHandler = (requestHandler) => {
+    return(req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+// try/catch method
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+//     } catch(error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
